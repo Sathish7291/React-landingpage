a3b2b0fa705a575dadf20eac47559e11c74f8be3
sathish7291 <sathish010395@gmail.com>
2024-07-17 22:06:08 +0530

    landingpage

--- src/components/Image_show.jsx
+++ src/components/Image_show.jsx
@@ -5,7 +5,7 @@ function Image_show({imagedata}) {
   return <>
        
                 <div className="row g-0">
-                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style={{backgroundImage:`url('${image}')`}}></div>
+                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style={{backgroundImage:`url(${image})`}}></div>
                     <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                         <h2>{title}</h2>
                         <p className="lead mb-0">{description}</p>
