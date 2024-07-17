794c656a9b9cd84bd51dfa6c5f8ab2568ea85826
sathish7291 <sathish010395@gmail.com>
2024-07-17 22:36:06 +0530

    landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Image_show from './components/Image_show'
 import Testimonial from './components/Testimonial'
 import CallofAction from './components/CallofAction'
 import Fotter from './components/Fotter'
+import image1 from '../src/assets/img/bg-showcase-1.jpg'
+import image2 from '../src/assets/img/bg-showcase-2.jpg'
+import image3 from '../src/assets/img/bg-showcase-3.jpg'
+import pic1 from '../src/assets/img/testimonials-3.jpg'
+import pic2 from '../src/assets/img/testimonials-2.jpg'
+import pic3 from '../src/assets/img/testimonials-1.jpg'
 
 function App() {
   let data = [
@@ -28,33 +34,33 @@ function App() {
     {
      title :"Fully Responsive Design",
       description :"When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!",
-      image : "../src/assets/img/bg-showcase-1.jpg"
+      image : image1
     },
     {
       title :"Updated For Bootstrap 5",
       description :"Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!",
-      image : "../src/assets/img/bg-showcase-2.jpg"
+      image : image2
     },
     {
       title :"Easy to Use & Customize",
       description :"Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!",
-      image : "../src/assets/img/bg-showcase-3.jpg"
+      image : image3
     }
   ]
   let testimonialdata = [
     {
-      pic:"../src/assets/img/testimonials-3.jpg",
+      pic: pic1,
       name:"Sarah W.",
       desc:"Thanks so much for making these free resources available to us!"
     },
     {
-        pic:"../src/assets/img/testimonials-2.jpg",
+        pic: pic2,
         name:"Fred S.",
         desc:"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."
     },
    
     {
-      pic:"../src/assets/img/testimonials-1.jpg",
+      pic: pic3,
       name:"Margaret E.",
       desc:"This is fantastic! Thanks so much guys!"
   }]
